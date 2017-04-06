14aa016e1c187670ec5a941544064b8ace1ad7be
Mati O <spirt.x@gmail.com>
2017-04-06 20:59:54 +0300

    pink circle

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-"" 
+
+
+window.onload = () =>{
+  const canvas =document.getElementById('canvas2d');
+  /**
+   * @type {CanvasRenderingContext2D}
+   */
+  const ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.strokeStyle = '#000000';
+  ctx.lineWidth = 4;
+  ctx.fillStyle = '#ff00ff';
+  ctx.ellipse(62,62,60,60,0,0,360,false);
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
+};
